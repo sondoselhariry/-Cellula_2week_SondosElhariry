def9d7966a16c2fd94b2d2d1e89c5e68f8af991a
sondoselhariry <167926020+sondoselhariry@users.noreply.github.com>
2025-10-29 01:04:35 +0200

    Add files via upload

--- image captioning/imagecaption.py
+++ image captioning/imagecaption.py
@@ -1,1 +1,30 @@
 
+
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import torch
+
+
+model_name = "Salesforce/blip-image-captioning-base"
+processor = BlipProcessor.from_pretrained(model_name)
+model = BlipForConditionalGeneration.from_pretrained(model_name)
+
+
+device = "cpu"
+
+model.to(device)
+
+def generate_caption(image_path):
+    # Open image
+    image = Image.open(image_path).convert('RGB')
+
+    # Preprocess
+    inputs = processor(images=image, return_tensors="pt").to(device)
+
+    # Generate
+    outputs = model.generate(**inputs, max_new_tokens=20)
+
+    # Decode
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
+
+    return caption
